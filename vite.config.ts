c23576b8f82d79fe0ad9a5b1ba748bd0a28aa85f
Yuji <user@yuji.local>
2026-06-06 17:34:57 +0800

    移除vite-plugin-cesium和cesium包，3D使用天地图CDN加载

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import cesium from 'vite-plugin-cesium'
 
 export default defineConfig({
-  plugins: [cesium()],
+  plugins: [],
   base: '/yuji-ancient-terrain/',
   server: {
     port: 5178,
